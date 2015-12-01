fd5c0b56281bad325e54ddecb871fc12c0c7cb52
JamieSinn <james.sinn@sinndevelopment.com>
2015-12-01 14:01:27 -0500

    Update to have year

--- TopVoters.py
+++ TopVoters.py
@@ -1,25 +1,29 @@
 import re
 import collections
-import datetime
+import time
+import sys
 
 __author__ = 'JamieSinn'
 # Use this with a Votifier log to get the top 10 voters each month
 # Replace the month with the month you want to check for
 
-month = '08'
+month = '10'
+year = '2015'
 
 
 def email():
     import smtplib
     from email.mime.text import MIMEText
     import time
-    __author__ = 'Jamie'
     USERNAME = "no-reply@sinndevelopment.com"
     PASSWORD = ""
     f = open('top10.txt', 'rb')
     msg = MIMEText(re.sub("(\('username:|'|\))", '', f.read().replace(', (', '\n(')))
     f.close()
-    msg['Subject'] = 'Monthly Voters for %s' % time.strftime("%m/%Y")
+    if sys.argv[1] == "test":
+        msg['Subject'] = 'TESTING MONTHLY VOTERS - NOT ACTUAL VOTES.'
+    else:
+        msg['Subject'] = 'Monthly Voters for %s' % time.strftime(int("%m")-1)
     msg['From'] = 'no-reply@sinndevelopment.com'
     msg['To'] = ''
     conn = smtplib.SMTP_SSL('p3plcpnl002.prod.phx3.secureserver.net', 465)
@@ -34,9 +38,11 @@ with open('votes.log', 'r') as File:
     votes = []
     content = File.readlines()
     for line in content:
-        date = datetime.datetime.fromtimestamp(int(re.search('timeStamp:\d+', line).group(0).replace("timeStamp:", ''))
-                                               ).strftime('%m')
-        if date == month:
+        dateM = time.gmtime(
+            int(re.search('timeStamp:\d+', line).group(0).replace("timeStamp:", ''))/1000).strftime('%m')
+        dateY = time.gmtime(
+            int(re.search('timeStamp:\d+', line).group(0).replace("timeStamp:", ''))/1000).strftime('%Y')
+        if dateM == month and dateY == year:
             votes.append(re.search('username:.* a', line).group(0).replace(" a", ''))
     with open('top10.txt', 'w') as out:
         out.write(', '.join(str(x) for x in collections.Counter(votes).most_common(10)))
